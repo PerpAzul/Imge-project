dcff8f08d7e67730882b8a3dc4f76085e879f59a
PerpAzul <felipe.lucas@outlook.com>
2024-01-21 18:52:33 +0100

    Tablets and car added to make way finding

---
+++ Imge Project/Assets/Scripts/Interactables/LetterInteract.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class LetterInteract : Interactable
+{
+    [SerializeField] private TextMeshProUGUI textToUpdate;
+    [SerializeField] private GameObject gameUI;
+    [SerializeField] private GameObject papers;
+    public GameObject Text;
+    private bool NomorePaper = false;
+
+    private void Awake()
+    {
+        Text.SetActive(false);
+    }
+
+    protected override void Interact()
+    {
+        if (!NomorePaper)
+        {
+            gameUI.SetActive(false);
+            papers.SetActive(true);
+            Text.SetActive(true);
+            NomorePaper = true;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            gameUI.SetActive(true);
+            papers.SetActive(false);
+            Text.SetActive(false);
+            NomorePaper = false;
+            Time.timeScale = 1;
+        }
+    }
+}
