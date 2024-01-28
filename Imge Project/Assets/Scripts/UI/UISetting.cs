25177bf75955240b8cd962ab18a6e30a68b32dba
23-Lukas <lukas.daniek@gmail.com>
2024-01-28 19:10:09 +0100

    Added change resolution to menu options

--- Imge Project/Assets/Scripts/UI/UISetting.cs
+++ Imge Project/Assets/Scripts/UI/UISetting.cs
@@ -10,14 +10,26 @@ public class UISetting : MonoBehaviour
     //sensitivity UI Text
     public TextMeshProUGUI SensitivityText;
     public TextMeshProUGUI VolumeText;
-
+    public TextMeshProUGUI ResolutionText;
     public Slider s;
     public Slider s_v;
     public Toggle fs_t;
+    private struct res {
+        public int width;
+        public int height;
+    }
+    private res[] Resolutions = new res[3];
+    private int selectedResolution = 1;
     // Update is called once per frame
     private void Awake()
     {
         fs_t.isOn = Screen.fullScreen;
+        Resolutions[0].width    = 1280;
+        Resolutions[0].height   =  720;
+        Resolutions[1].width    = 1920;
+        Resolutions[1].height   = 1080;
+        Resolutions[2].width    = 2560;
+        Resolutions[2].height   = 1440; 
     }
 
     void Update()
@@ -42,6 +54,41 @@ public class UISetting : MonoBehaviour
         Screen.fullScreen = fs_t.isOn;
     }
 
+    public void resolutionLeft()
+    {
+        if (selectedResolution > 0) {
+            selectedResolution--;
+        }
+        else
+        {
+            selectedResolution = 0;
+        }
+        SetResolution(Resolutions[selectedResolution].width, Resolutions[selectedResolution].height);
+        updateResolutionText();
+    }
+    public void resolutionRight()
+    {
+        if (selectedResolution < Resolutions.Length - 2) 
+        {
+            selectedResolution++;
+        }
+        else
+        {
+            selectedResolution = (Resolutions.Length - 1);
+        }
+        SetResolution(Resolutions[selectedResolution].width, Resolutions[selectedResolution].height);
+        updateResolutionText();
+    }
+    private void updateResolutionText()
+    {
+        ResolutionText.text = Resolutions[selectedResolution].width + "x" + Resolutions[selectedResolution].height;
+    }
+    public void SetResolution(int width, int height)
+    {
+        bool fs = Screen.fullScreen;
+        var screenRefreshRate = 144;
+        Screen.SetResolution(width, height, fs, screenRefreshRate);
+    }
     void OnEnable()
     {
         s.value = PlayerLook.sensitivityScale;
